a8d84464687a55fe33507640f42f4f55edb8291f
Keith Ashorn <ashorn.keith@utexas.edu>
2014-04-29 16:40:25 -0500

    commented https redirect

--- closeAuction.php
+++ closeAuction.php
@@ -1,11 +1,12 @@
 <?php
 require 'db.php';
 
+/*
 if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') { 
 	// we're not running on https
 	header('Location: https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
 	exit (0);
-} 
+} */
 
 $expiredAuctionsQuery = $database->prepare('
 	SELECT
