4503f3d9802e3e4d5f8762a730950e3aed03b6f9
Keith Ashorn <ashorn.keith@utexas.edu>
2014-04-29 16:15:59 -0500

    added db setup

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+try {
+	$info = parse_url(getenv('DATABASE_URL'));
+    $database = new PDO("phsql:host={$info['host']};port={$info['port']};dbname={$info['path']}", $info['user'], $info['pass'], array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => true
+    ));
+} catch(PDOException $e) {
+    error_log("{$e->getFile()}:{$e->getLine()}: PDO open failed: {$e->getCode()}: {$e->getMessage()}");
+    header("HTTP/1.1 500 Internal Server Error");
+    echo '<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <title>Internal Server Error</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Sorry, this Web site has encountered an unexpected condition, and is currently unable to respond to your request.</p>
+    <p>Please retry later.</p>
+    <p><small></small></p>
+  </body>
+</html>
+';
+    exit(1);
+}
+?>
+
